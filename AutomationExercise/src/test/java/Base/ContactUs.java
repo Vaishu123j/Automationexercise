bad1494da56d19559159f2d11bcf96684040c386
Vaishnavi jadhav <149877484+Vaishu123j@users.noreply.github.com>
2023-11-24 16:17:08 +0530

    Update ContactUs.java

--- AutomationExercise/src/test/java/Base/ContactUs.java
+++ AutomationExercise/src/test/java/Base/ContactUs.java
@@ -5,12 +5,51 @@ import org.openqa.selenium.support.FindBy;
 
 public class ContactUs {
 	
-	@FindBy(xpath="//*[@href='/contact_us']") public WebElement contactus;
-	@FindBy(xpath="//*[@class='btn btn-success']") public WebElement homepage;
-	@FindBy(name = "name") public WebElement Name;
-	@FindBy(name = "email") public WebElement Email;
-	@FindBy(name ="subject")public WebElement Subject;
-	@FindBy(name ="message")public WebElement Message;
-	@FindBy(name="submit")public WebElement Submit;
+	 WebDriver driver;
 
-}
+    
+    public ContactUs(WebDriver driver) {
+        this.driver = driver;
+    }
+	
+	@FindBy(xpath="//*[@href='/contact_us']")  WebElement contactus;
+	@FindBy(xpath="//*[@class='btn btn-success']")  WebElement homePage;
+	@FindBy(name = "name")  WebElement Name;
+	@FindBy(name = "email")  WebElement Email;
+	@FindBy(name ="subject") WebElement Subject;
+	@FindBy(name ="message") WebElement Message;
+	@FindBy(name="submit") WebElement Submit;
+	
+	 public void clickContactUsLink() {
+	        contactus.click();
+	    }
+
+	    public void clickHomePageButton() {
+	        homePage.click();
+	    }
+
+	    public void enterName(String name) {
+	        Name.sendKeys(name);
+	    }
+
+	    public void enterEmail(String email) {
+	        Email.sendKeys(email);
+	    }
+
+	    public void enterSubject(String subject) {
+	        Subject.sendKeys(subject);
+	    }
+
+	    public void enterMessage(String message) {
+	        Message.sendKeys(message);
+	    }
+
+	    public void clickSubmit() {
+	        Submit.click();
+	    }
+
+		
+
+		
+		
+	}
