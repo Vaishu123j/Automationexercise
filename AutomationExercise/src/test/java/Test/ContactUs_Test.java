152f90be488fd89c24a59aa2ae0039679ce056cd
Vaishnavi jadhav <149877484+Vaishu123j@users.noreply.github.com>
2023-11-24 16:25:29 +0530

    Update ContactUs_Test.java

--- AutomationExercise/src/test/java/Test/ContactUs_Test.java
+++ AutomationExercise/src/test/java/Test/ContactUs_Test.java
@@ -13,44 +13,43 @@ import Base.BaseTest;
 import Base.ContactUs;
 
 public class ContactUs_Test extends BaseTest{
-	WebDriver driver; 
-	
+	WebDriver driver;
+
 	@BeforeMethod
 	public void Initialization() {
 		driver = intialization("Chrome");
 	}
-	
+
 	@Test
 	public void contact_us() throws Exception, Exception {
-	ContactUs p = PageFactory.initElements(driver, ContactUs.class);
-	p.contactus.click();
-	boolean getintouch =driver.findElement(By.xpath("//*[text()='Get In Touch']")).isDisplayed();
-	if(getintouch=true) {
-		System.out.println(getintouch + ": "+ driver.findElement(By.xpath("//*[text()='Get In Touch']")).getText());
+		ContactUs p = PageFactory.initElements(driver, ContactUs.class);
+		p.clickContactUsLink();
+		boolean getintouch = driver.findElement(By.xpath("//*[text()='Get In Touch']")).isDisplayed();
+		if (getintouch = true) {
+			System.out
+					.println(getintouch + ": " + driver.findElement(By.xpath("//*[text()='Get In Touch']")).getText());
+		} else {
+			System.out.println("Not displayed");
 		}
-	else {
-		System.out.println("Not displayed");
-	}
-	p.Name.sendKeys("xyz");
-	p.Email.sendKeys("xtz@gmail.com");
-	p.Subject.sendKeys("product return");
-	p.Message.sendKeys("I want to return this product");
-	p.Submit.click();
-	Alert a = driver.switchTo().alert();
-	a.accept();
-	boolean successmsg =driver.findElement(By.xpath("//*[@class='status alert alert-success']")).isDisplayed();
-	if(successmsg=true) {
-		System.out.println(successmsg + ": "+ driver.findElement(By.xpath("//*[@class='status alert alert-success']")).getText());
+		p.enterMessage("xyz");
+		p.enterEmail("xtz@gmail.com");
+		p.enterSubject("product return");
+		p.enterMessage("I want to return this product");
+		p.clickSubmit();
+		Alert a = driver.switchTo().alert();
+		a.accept();
+		boolean successmsg = driver.findElement(By.xpath("//*[@class='status alert alert-success']")).isDisplayed();
+		if (successmsg = true) {
+			System.out.println(successmsg + ": "
+					+ driver.findElement(By.xpath("//*[@class='status alert alert-success']")).getText());
+		} else {
+			System.out.println("Not displayed");
 		}
-	else {
-		System.out.println("Not displayed");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		p.clickHomePageButton();
+		boolean homepage = driver.findElement(By.xpath("//*[text()=' Home']")).isEnabled();
+		System.out.println(homepage + ": " + driver.findElement(By.xpath("//*[text()=' Home']")).getText());
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	p.homepage.click();
-	boolean homepage = driver.findElement(By.xpath("//*[text()=' Home']")).isEnabled();
-	System.out.println(homepage + ": " + driver.findElement(By.xpath("//*[text()=' Home']")).getText());
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-}
-
 
 }
