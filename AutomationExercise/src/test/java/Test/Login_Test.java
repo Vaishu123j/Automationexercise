4b4aee7b88865194531014972daeb057bf57425a
Vaishnavi jadhav <149877484+Vaishu123j@users.noreply.github.com>
2023-11-24 16:28:12 +0530

    Update Login_Test.java

--- AutomationExercise/src/test/java/Test/Login_Test.java
+++ AutomationExercise/src/test/java/Test/Login_Test.java
@@ -3,7 +3,6 @@ import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.Alert;
@@ -17,40 +16,38 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.microsoft.schemas.office.visio.x2012.main.SheetType;
-
 import Base.BaseTest;
 import Base.Login;
 
+
 public class Login_Test extends BaseTest{
-	private static final String Workbook = null;
 	WebDriver driver;
 	Login p;
-	
+
 	@BeforeMethod
 	public void SetUp() {
 		driver = intialization("Chrome");
-		
+
 	}
-	
-	@Test(priority=1)
+
+	@Test(priority = 1)
 	public void registerUser() {
- 		Login p = PageFactory.initElements(driver, Login.class);
-		p.login.click();
-	    p.name.sendKeys("Test");
-		p.emailid.sendKeys("testcheck7899@gmail.com");
-		p.btn.click();
-		
+		Login p = PageFactory.initElements(driver, Login.class);
+		p.clickonLogin();
+		p.enternamefield("Test");
+		p.enteremailid("testcheck7899@gmail.com");
+		p.clickonbutton();
+
 		boolean eai = driver.findElement(By.xpath("//*[text()='Enter Account Information']")).isDisplayed();
-		if(eai=true) {
-			System.out.println(eai + " " + driver.findElement(By.xpath("//*[text()='Enter Account Information']")).getText());
-		}
-		else {
+		if (eai = true) {
+			System.out.println(
+					eai + " " + driver.findElement(By.xpath("//*[text()='Enter Account Information']")).getText());
+		} else {
 			System.out.println("Not displayed");
 		}
-		p.title.click();
-		p.psd.sendKeys("Test789@");
+		p.clickontitle();
+		p.clickonpassword("Test789@");
 		WebElement day = driver.findElement(By.id("days"));
 		Select obj1 = new Select(day);
 		obj1.selectByValue("29");
@@ -60,88 +57,84 @@ public class Login_Test extends BaseTest{
 		WebElement years = driver.findElement(By.id("years"));
 		Select obj3 = new Select(years);
 		obj3.selectByValue("1995");
-		JavascriptExecutor js =(JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,360)", "");
-		p.chk1.click();
-		p.chk2.click();
-		p.firstname.sendKeys("Check1");
-		p.lastname.sendKeys("Check2");
+		p.clickoncheckbox1();
+		p.clickoncheckbox2();
+		p.enterfirstnamek("Check1");
+		p.enterlastname("Check2");
 		p.company.sendKeys("Software Testing");
-		p.addrs1.sendKeys("789,near pheonix mall");
-		p.addrs2.sendKeys("Street no. 5, Software Testing");
+		p.enteraddress("789,near pheonix mall");
+		p.enteraddress2("Street no. 5, Software Testing");
 		WebElement country = driver.findElement(By.id("country"));
 		Select obj4 = new Select(country);
 		obj4.selectByVisibleText("India");
-		p.state.sendKeys("Maharahtra");
-		p.city.sendKeys("Pune");
-		p.zipcode.sendKeys("411048");
-		p.mobnumber.sendKeys("1234567891");
-		p.createaccbtn.click();
-		boolean accc= driver.findElement(By.xpath("//*[text()='Account Created!']")).isDisplayed();
-		if(accc=true) {
-			System.out.println(accc + " "+ driver.findElement(By.xpath("//*[text()='Account Created!']")).getText() );
-			}
-		else {
+		p.enterstate("Maharahtra");
+		p.entercity("Pune");
+		p.enterzipcode("411048");
+		p.entermobilenumber("1234567891");
+		p.clickoncreateaccount();
+		boolean accc = driver.findElement(By.xpath("//*[text()='Account Created!']")).isDisplayed();
+		if (accc = true) {
+			System.out.println(accc + " " + driver.findElement(By.xpath("//*[text()='Account Created!']")).getText());
+		} else {
 			System.out.println("Not displayed");
 		}
-		p.continu.click();
+		p.clickoncontinuebutton();
 		boolean logged = driver.findElement(By.xpath("//*[contains(@class,'fa fa-user')]")).isDisplayed();
-		if(logged=true) {
-			System.out.println(logged + " "+ driver.findElement(By.xpath("//*[contains(@class,'fa fa-user')]")).getText());
-			}
-		else {
+		if (logged = true) {
+			System.out.println(
+					logged + " " + driver.findElement(By.xpath("//*[contains(@class,'fa fa-user')]")).getText());
+		} else {
 			System.out.println("Not displayed");
 		}
-		p.delacc.click();
-		boolean delact =driver.findElement(By.xpath("//*[text()='Account Deleted!']")).isDisplayed();
-		if(delact=true) {
-			System.out.println(delact + " "+ driver.findElement(By.xpath("//*[text()='Account Deleted!']")).getText());
-			}
-		else {
+		p.clickondelaccount();
+		boolean delact = driver.findElement(By.xpath("//*[text()='Account Deleted!']")).isDisplayed();
+		if (delact = true) {
+			System.out.println(delact + " " + driver.findElement(By.xpath("//*[text()='Account Deleted!']")).getText());
+		} else {
 			System.out.println("Not displayed");
 		}
-		p.continu.click();
-		}
-		
-	
+		p.clickoncontinuebutton();
+	}
+
 	@Test
 	public void login_ValidUser() throws Exception {
 		Login p = PageFactory.initElements(driver, Login.class);
-		p.name.sendKeys("Test");
-		p.psd.sendKeys("Test789@");
-		boolean loginacc =driver.findElement(By.xpath("//*[text()='Login to your account']")).isDisplayed();
-		if(loginacc=true) {
-			System.out.println(loginacc + " "+ driver.findElement(By.xpath("//*[text()='Login to your account']")).getText());
-			}
-		else {
+		p.enternamefield("Test");
+		p.clickonpassword("Test789@");
+		boolean loginacc = driver.findElement(By.xpath("//*[text()='Login to your account']")).isDisplayed();
+		if (loginacc = true) {
+			System.out.println(
+					loginacc + " " + driver.findElement(By.xpath("//*[text()='Login to your account']")).getText());
+		} else {
 			System.out.println("Not displayed");
-		}	
-		
+		}
+
 	}
-	@Test(priority=3)
+
+	@Test(priority = 3)
 	public void login_InvalidUser() throws Exception {
 		Login p = PageFactory.initElements(driver, Login.class);
-		p.login.click();
+		p.clickonLogin();
 		driver.findElement(By.name("email")).sendKeys("testcheck7891@gmail.com");
 		driver.findElement(By.name("password")).sendKeys("Test789@");
 		driver.findElement(By.xpath("//*[text()='Login']")).click();
-	
-	boolean msg = driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']")).isDisplayed();
-	if(msg=true) {
-		System.out.println(msg + ": "+ driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']")).getText());
+
+		boolean msg = driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']")).isDisplayed();
+		if (msg = true) {
+			System.out.println(msg + ": "
+					+ driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']")).getText());
+		} else {
+			System.out.println("Not displayed");
 		}
-	else {
-	System.out.println("Not displayed");
+		driver.close();
 	}
-	driver.close();
-	}
-	
+
 	@AfterMethod
-	
+
 	public void TearDown() {
 		driver.quit();
 	}
-	
-	
 
 }
